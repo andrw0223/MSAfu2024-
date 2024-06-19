4d4b54ae1052da2aa0e3c3e1efb826a4e1b0632f
andrw0223 <andrewfu2007@gmail.com>
2024-06-19 09:15:45 -0500

    Update math_interpreter.py

--- MSA/math_interpreter.py
+++ MSA/math_interpreter.py
@@ -1,34 +1,44 @@
 def main():
-    expression = input("Expression: ").split(" ")
-    if (not len(expression) == 3):
-        print("ERROR: Please enter in 3 characters seperated by a space")
+    
+    while(True):
+        expression = input("Expression: ").split(" ")
+        if (not len(expression) == 3):
+            print("ERROR: Please enter in 3 characters seperated by a space")
+            continue
+        try:    
+            a = float(expression[0])
+            b = float(expression[2])
 
-        a = float(expression[0])
-
-    sign = expression[1]
-    if (sign == '+'):
-        answer = float(expression[0] + expression[2])
-        print(f"{answer}")
+            sign = expression[1]
+            
+            if (sign == '+'):
+                answer = (a + b)
+                print(f"{answer}")
+                break
+        
+            if (sign == '-'):
+                answer = (a - b)
+                print(f"{answer}")
+                break
+        
+        
+            if (sign == '*'):
+                answer = (a * b)
+                print(f"{answer}")
+                break
+        
+        
+            if (sign == '/'):
+                if (b == 0):
+                    print("Division by 0 is not allowed")
+                    continue
+                answer = (a / b)
+                print(f"{answer:.2f}")
+                break
+            else:
+                print("ERROR: Please enter a operator in the second character")
     
-    if (sign == '-'):
-        answer = float(expression[0] - expression[2])
-        print(f"{answer}")
-    
-    
-    if (sign == '*'):
-        answer = float(expression[0] * expression[2])
-        print(f"{answer}")
-    
-    
-    if (sign == '/'):
-        answer = float(expression[0] / expression[2])
-        print(f"{answer}")
-    else:
-        print("ERROR: Please enter a operator in the second character")
-    
-    
-
-
-
+        except:
+            print("ERROR: PLease eneter 2 integers and an operator all seperated by a space")
 
 main()
